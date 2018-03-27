411223c7f30d9f02a9dc28b1c66fdbeb75a56ea0
Grand Circus <yabd89@gmail.com>
2018-03-27 11:40:32 -0400

    fixed max-width on container and animations at screen sizes

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-$( document ).ready(function() {
-    $(".project-section-image").on("mouseover", function(){
-        $(this).addClass("swing animated");
-        $(this)
-    }).on("mouseout", function(){
-        $(this).removeClass("swing animated");
-    })
+
+
+$(document).ready(function() {
+
+    var windowWidth = $(window).width();
+    console.log(windowWidth);
+    
+    if(windowWidth > 768) {
+        $(".project-section-image").on("mouseover", function(){
+            $(this).addClass("swing animated");
+            $(this)
+        }).on("mouseout", function(){
+            $(this).removeClass("swing animated");
+        })
+    }
     
 });
